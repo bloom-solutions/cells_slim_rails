85bc1722321829819ce06fa6fcce0214a52abb48
AJ Villalobos <hi@ajvillalobos.com>
2018-07-10 17:25:45 +0800

    Overwrite defaults for cells-slim

--- lib/cells_slim_rails.rb
+++ lib/cells_slim_rails.rb
@@ -1,5 +1,17 @@
 require "cells_slim_rails/version"
+require "cell/slim"
 
-module CellsSlimRails
-  # Your code goes here...
+module Cell
+  module Slim
+    def template_options_for(_options)
+      {
+        template_class: ::Slim::Template,
+        suffix:         "slim",
+        disable_escape: false,
+        escape_code:    false,
+        use_html_safe:  true,
+        buffer:         "@output_buffer"
+      }
+    end
+  end
 end
